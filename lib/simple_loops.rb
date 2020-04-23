2e290325835fca75122c41f5685c18d2cb380dfa
Felix Rodriguez <frod25@gmail.com>
2020-04-23 18:42:40 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,3 +5,10 @@ def loop_message_five_times(message)
     count += 1
   end
 end
+
+def loop_message_n_times(message, output)
+  count = 0
+  while count < output do
+    puts message
+    count += 1
+  end
